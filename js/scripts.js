a380538cc26e3bac2b96b6948e259020da732f01
hayley <hluhn@engr-131-11.eastern.ewu.edu>
2014-02-20 14:23:11 -0800

    Finished for now

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 // Make Foundation Go!
-$(document).foundation();
+//$(document).foundation();
 
 // Your Awesome Scripts!
 $(document).ready(function(){
+
+
+// good stuff
+new Maplace({
+	show_markers: true,
+	locations: [{
+		lat: 45.9, 
+		lon: 10.9,
+		zoom: 8
+	}]
+}).Load(); 
 
 	console.log('Page Loaded. Lets Do this!');
 
